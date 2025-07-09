d9f80c9e93ea3d1c3660c942073e890d56058463
fe-i <114176049+fe-i@users.noreply.github.com>
2025-07-09 14:15:52 -0400

    add shadcn ui

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { ColorModeToggle } from "@/components/color-mode-toggle";
+
+export default async function Dashboard() {
+  return (
+    <main className="">
+      <ColorModeToggle />
+    </main>
+  );
+}
